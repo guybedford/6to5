ca7ea081a4af2a68e7a3d28984852f0cf03a2524
guybedford <guybedford@gmail.com>
2016-01-06 17:28:17 +0200

    ensure regenerator: false, polyfill: true works correctly

--- packages/babel-plugin-transform-runtime/src/index.js
+++ packages/babel-plugin-transform-runtime/src/index.js
@@ -24,11 +24,9 @@ export default function ({ types: t }) {
 
     visitor: {
       ReferencedIdentifier(path, state) {
-        if (state.opts.regenerator === false) return;
-        
         let { node, parent, scope } = path;
 
-        if (node.name === "regeneratorRuntime") {
+        if (state.opts.regenerator !== false && node.name === "regeneratorRuntime") {
           path.replaceWith(state.get("regeneratorIdentifier"));
           return;
         }
